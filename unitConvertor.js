18b89fa6aecd214546be24d18eb3a3bd4c83cb26
Yiwen Shi <shiyiwenbupt@gmail.com>
2015-10-31 11:28:07 -0500

    v1.0

---
+++ unitConvertor.js
@@ -0,0 +1,62 @@
+var UnitConvertorApp = angular.module('UnitConvertorApp', ['ngRoute']);
+//var UnitConvertorApp = angular.module('UnitConvertorApp', []);
+UnitConvertorApp.config(function($routeProvider) {
+  $routeProvider.
+    when('/', {
+      templateUrl: 'welcome.html'
+    }).
+    when('/:UnitType', {
+      templateUrl: 'unitDetail.html',
+      controller: 'UnitDetailCtrl'
+    }).
+    otherwise({
+      redirectTo: '/'
+    });
+});
+
+UnitConvertorApp.controller('IndexCtrl', function ($scope) {
+  $scope.UnitTypes = ['Length','Weight', 'Area', 'Volume', 'Speed'];
+  $scope.InitClass = function() {
+    for(var i=0;i<5;i++) {
+      $('#link-'+$scope.UnitTypes[i]).removeClass();
+    }
+  }
+  $scope.InitClass();
+  $scope.changeUnit = function(unitType) {
+    //console.log(unitType);
+    $scope.InitClass();
+    $('#link-'+unitType).addClass("selected");
+    //$scope.valueChange(unitValue, newUnit.toStandard);
+  };
+});
+
+
+
+UnitConvertorApp.controller('UnitDetailCtrl', function ($scope, $routeParams, $http){
+  $scope.unittype=$routeParams.UnitType;
+  //console.log($routeParams);
+  $('#link-'+$scope.unittype).addClass("selected");
+  $http.get('/file/'+$routeParams.UnitType+'.json').success(function(data) {
+    $scope.units=data;
+    $scope.unitSelected0 = data[0];
+    $scope.unitSelected1 = data[1];
+    $scope.unitSelected2 = data[2];
+    $scope.unitSelected3 = data[3];
+    $scope.unitValue0 = 1 / $scope.unitSelected0.toStandard;
+    $scope.unitValue1 = 1 / $scope.unitSelected1.toStandard;
+    $scope.unitValue2 = 1 / $scope.unitSelected2.toStandard;
+    $scope.unitValue3 = 1 / $scope.unitSelected3.toStandard;
+  });
+  $scope.valueChange = function(unitValue, StandardSelected) {
+    $scope.unitValue0 = unitValue * StandardSelected / $scope.unitSelected0.toStandard;
+    $scope.unitValue1 = unitValue * StandardSelected / $scope.unitSelected1.toStandard;
+    $scope.unitValue2 = unitValue * StandardSelected / $scope.unitSelected2.toStandard;
+    $scope.unitValue3 = unitValue * StandardSelected / $scope.unitSelected3.toStandard;
+  };
+  $scope.unitChange = function(unitValue, newUnit) {
+    //console.log(unitValue);
+    $scope.valueChange(unitValue, newUnit.toStandard);
+  };
+});
+
+
