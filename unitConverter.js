bcb8cd3539fd20d95f83c942b34e1f1e7bc27f94
Yiwen Shi <shiyiwenbupt@gmail.com>
2015-11-01 00:01:34 -0500

    update

--- unitConverter.js
+++ unitConverter.js
@@ -52,10 +52,42 @@ UnitConverterApp.controller('UnitDetailCtrl', function ($scope, $routeParams, $h
     $scope.unitValue1 = unitValue * StandardSelected / $scope.unitSelected1.toStandard;
     $scope.unitValue2 = unitValue * StandardSelected / $scope.unitSelected2.toStandard;
     $scope.unitValue3 = unitValue * StandardSelected / $scope.unitSelected3.toStandard;
+
+          // var arr = String($scope.unitValue0).split("");
+          // var countdot=0;
+          // arr.forEach(function(entry) {
+          //     if(entry==='.')
+          //       countdot+=1;
+          // });
+          // console.log(countdot);
   };
   $scope.unitChange = function(unitValue, newUnit) {
     //console.log(unitValue);
     $scope.valueChange(unitValue, newUnit.toStandard);
+  };
+});
+
+UnitConverterApp.directive('isNumber', function () {
+  return {
+    require: 'ngModel',
+    link: function (scope) {  
+      scope.$watch('unitValue0', function(newValue,oldValue) {
+          var arr = String(newValue).split("");
+          if (arr.length === 0) return;
+          if (arr.length === 1 && (arr[0] == '-' || arr[0] === '.' )) return;
+          if (arr.length === 2 && newValue === '-.') return;
+          var countdot=0;
+          arr.forEach(function(entry) {
+              if(entry==='.')
+                countdot+=1;
+          });
+          if (countdot<2) return;
+          console.log(countdot);
+          if (isNaN(newValue)) {
+              scope.unitValue0 = oldValue;
+          }
+      });
+    }
   };
 });
 
